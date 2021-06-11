4625986c74aacb2c712a370471555ad13ea8266f
José Miguel Subelza Alvarez <jose_miguels@hotmail.com>
2021-06-11 08:31:15 -0400

    Cambios estructura

--- NotasSL/core/colegio/models.py
+++ NotasSL/core/colegio/models.py
@@ -14,3 +14,18 @@ class Gestion(models.Model):
         verbose_name = 'Gestión'
         verbose_name_plural = 'Gestiones'
         ordering = ['year']
+
+
+class GestionRango(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Descripción', null=False)
+    fecha_inicio = models.DateTimeField(verbose_name='Fecha Inicio', null=False)
+    fecha_fin = models.DateTimeField(verbose_name='Fecha Final', null=False)
+    gestion = models.ForeignKey(Gestion, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+    class Meta:
+        verbose_name = 'Gestión'
+        verbose_name_plural = 'Gestiones'
+
